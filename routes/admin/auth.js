51caae5e457d9ec48b7f6bfb96b555149f6e5273
Ricardo <francoliricardo@gmail.com>
2020-07-01 17:01:53 +0100

    add edit functionality

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -21,22 +21,16 @@ router.get('/signup', (req, res) => {
 router.post(
 	'/signup',
 	[requireEmail, requirePassword, requirePasswordConfirmation], // express-validator here
+	handleErrors(signupTemplate),
 	async (req, res) => {
-		const errors = validationResult(req);
-
-		if (!errors.isEmpty()) {
-			return res.send(signupTemplate({ req, errors }));
-		}
-
-		const { email, password, passwordConfirmation } = req.body;
-
+		const { email, password } = req.body;
 		// Create a user in our user repo to represent this person
 		const user = await usersRepo.create({ email, password });
 
 		// Store the id of that user inside the user cookie
 		req.session.userId = user.id; // Added by the cookie session! userId could be anything (it's an object)
 
-		res.send('Account created!!!');
+		res.redirect('/admin/products');
 	}
 );
 
@@ -52,20 +46,15 @@ router.get('/signin', (req, res) => {
 router.post(
 	'/signin',
 	[requireEmailExists, requireValidPasswordForUser],
+	handleErrors(signinTemplate),
 	async (req, res) => {
-		const errors = validationResult(req);
-
-		if (!errors.isEmpty()) {
-			return res.send(signinTemplate({ errors }));
-		}
-
 		const { email } = req.body;
 
 		const user = await usersRepo.getOneBy({ email });
 
 		req.session.userId = user.id;
 
-		res.send('You are signed in!!!');
+		res.redirect('/admin/products');
 	}
 );
 
