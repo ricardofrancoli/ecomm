8dfcb9b81c3f3a2a1e46dea247dd6e972742b1a1
Ricardo <francoliricardo@gmail.com>
2020-07-03 17:41:47 +0100

    add edit, delete functionality

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -1,12 +1,18 @@
 const { validationResult } = require('express-validator');
 
 module.exports = {
-	handleErrors(templateFunc) {
-		return (req, res, next) => {
+	// dataCb, second argument, just for error handling on products.js
+	handleErrors(templateFunc, dataCb) {
+		return async (req, res, next) => {
 			const errors = validationResult(req);
 
 			if (!errors.isEmpty()) {
-				return res.send(templateFunc({ errors }));
+				let data = {};
+				if (dataCb) {
+					data = await dataCb(req);
+				}
+				// merge data object with errors
+				return res.send(templateFunc({ errors, ...data }));
 			}
 
 			next();
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -56,7 +56,37 @@ router.get(
 router.post(
 	'/admin/products/:id/edit',
 	requireAuth,
-	async (req, res) => {}
+	upload.single('image'),
+	[requireTitle, requirePrice],
+	handleErrors(productsEditTemplate, async (req) => {
+		const product = await productsRepo.getOne(req.params.id);
+		return { product };
+	}),
+	async (req, res) => {
+		const changes = req.body;
+
+		if (req.file) {
+			changes.image = req.file.buffer.toString('base64');
+		}
+
+		try {
+			await productsRepo.update(req.params.id, changes);
+		} catch (err) {
+			return res.send('Could not find intem');
+		}
+
+		res.redirect('/admin/products');
+	}
+);
+
+router.post(
+	'/admin/products/:id/delete',
+	requireAuth,
+	async (req, res) => {
+		await productsRepo.delete(req.params.id);
+
+		res.redirect('/admin/products');
+	}
 );
 
 module.exports = router;
